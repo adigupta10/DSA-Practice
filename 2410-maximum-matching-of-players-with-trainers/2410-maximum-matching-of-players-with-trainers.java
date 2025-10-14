6c2b826eb337900ff843c0f1c69e265128f470a0
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-10-14 23:27:45 +0530

    Time: 26 ms (63.43%), Space: 57.6 MB (78.98%) - LeetHub

---
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        int j=0;
+        int i=0;
+        int count=0;
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        while(i<players.length && j<trainers.length){
+            if(trainers[j]>=players[i]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
