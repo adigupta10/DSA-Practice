683a2da99e29bf2f17cc55e22fdbe3ba614e102f
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-10-22 15:06:52 +0530

    Time: 76 ms (11.04%), Space: 44.8 MB (100%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         ArrayList<Integer> list = new ArrayList<>();
-
-        // Step 1: Duplicate array (to simulate circular behavior)
         for (int num : nums) list.add(num);
-        for (int num : nums) list.add(num); // duplicate once
+        for (int num : nums) list.add(num);
 
         int[] res = new int[n];
-
-        // Step 2: Find next greater for each element
-        for (int i = 0; i < n; i++) {
-            res[i] = -1;
-            for (int j = i + 1; j < i + n; j++) { // look ahead n elements
-                if (list.get(j) > nums[i]) {
-                    res[i] = list.get(j);
+        for (int i=0;i<n;i++) {
+            res[i]=-1;
+            for (int j=i+1;j<i+n;j++){
+                if(list.get(j)>nums[i]) {
+                    res[i]=list.get(j);
                     break;
                 }
             }
         }
-
         return res;
     }
 }
