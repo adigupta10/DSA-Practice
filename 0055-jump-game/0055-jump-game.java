b1876a4e9b8a9272f11366f46feeb0f0a5ce0e92
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-11-05 16:37:51 +0530

    Time: 3 ms (38.56%), Space: 47.7 MB (5.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,20 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int finalposition=nums.length-1;
-        for(int i=nums.length-2;i>=0;i--){
-            if(i+nums[i]>=finalposition){
-                finalposition=i;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max){
+                return false;
             }
+            max=Math.max(max,nums[i]+i);
         }
-        return finalposition==0 ;
+        return true;
     }
 }
+
+//  int finalposition=nums.length-1;
+//         for(int i=nums.length-2;i>=0;i--){
+//             if(i+nums[i]>=finalposition){
+//                 finalposition=i;
+//             }
+//         }
+//         return finalposition==0 ;
