f75858c809001e796f14cfdc9345879438176fac
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-11-09 12:02:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,21 +1,18 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int start = 0,end = 0;
-        int n = fruits.length,maxLen = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        while(end<n)
-        {
-            map.put(fruits[end],map.getOrDefault(fruits[end],0)+1);
-            while(map.size()>=3)
-            {
-                map.put(fruits[start],map.get(fruits[start])-1);
-                if(map.get(fruits[start]) == 0) map.remove(fruits[start]);
-                start++;
+      int maxi=0;
+      for(int i=0;i<fruits.length;i++){
+        HashMap<Integer,Integer> map=new HashMap<>();
+            for(int j=i;j<fruits.length;j++){
+                map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+                if(map.size()<=2){
+                    maxi=Math.max(maxi,j-i+1);
+                }
+                else{
+                    break;
+                }
             }
-            int currLen = end-start+1;
-            maxLen = Math.max(maxLen,currLen);
-            end++;
         }
-        return maxLen;
+        return maxi;
     }
 }
