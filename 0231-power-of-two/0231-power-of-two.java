7c942d8bdce672086631b728842f1c97ddccea6f
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-11-26 01:21:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for(int i=0;i<n/2;i++){
+            if(Math.pow(2,i)==n){
+                return true;
+            }
+        }
+        return false;
+    }
+}
