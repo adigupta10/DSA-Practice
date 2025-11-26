59c6d1c4fa7f4a731ec6bfab151f6300151219ec
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-11-27 03:06:53 +0530

    Time: 1 ms (55.26%), Space: 42.8 MB (9.67%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,10 +1,11 @@
 class Solution {
+    public boolean ans(int n){
+        if(n==1) return true;
+        if(n<=0) return false;
+        if(n%2==0) return ans(n/2);
+        else return false;
+    }
     public boolean isPowerOfTwo(int n) {
-        for(int i=0;i<n/2;i++){
-            if(Math.pow(2,i)==n){
-                return true;
-            }
-        }
-        return false;
+        return ans(n);
     }
 }
