46d62d1749f9640036c1b3945f4dc7270cf36bb4
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2025-12-09 01:38:31 +0530

    Time: 1 ms (92.42%), Space: 46.5 MB (14.8%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -0,0 +1,53 @@
+class Solution {
+    private boolean solve(int[][] grid, int row,int col,int num){
+        int n=grid.length;
+        if(grid[row][col]==n*n-1) return true;
+        int i,j;
+        //2 up and 1 right
+        i=row-2;
+        j=col+1;
+        if(i>=0 && j<n && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        //2 up and 1 left
+        i=row-2;
+        j=col-1;
+        if(i>=0 && j>=0 && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        // 2 down and 1 right
+        i=row+2;
+        j=col+1;
+        if(i<n && j<n && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        //2 down and 1 left
+        i=row+2;
+        j=col-1;
+        if(i<n && j>=0 && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        //2 right and 1 up
+        i=row-1;
+        j=col+2;
+        if(i>=0 && j<n && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        //2 right and 1 down
+        i=row+1;
+        j=col+2;
+        if(i<n && j<n && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        // 2 left and 1 up
+        i=row-1;
+        j=col-2;
+        if(i>=0 && j>=0 && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+
+        //2 left and 1 down
+        i=row+1;
+        j=col-2;
+        if(i<n && j>=0 && grid[i][j]==num+1) return solve(grid,i,j,num+1);
+        return false;
+    }
+
+    public boolean checkValidGrid(int[][] grid) {
+        if(grid[0][0]!=0) return false;
+        return solve(grid,0,0,0);
+
+    }
+}
