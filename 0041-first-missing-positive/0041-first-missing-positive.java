fba8208a4fdfbc6150b697f35902190aee8af7df
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-01-14 16:10:32 +0530

    Time: 18 ms (13.83%), Space: 74 MB (29.13%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,23 +1,32 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n=nums.length;
-        int j=0;
-        while(j<n){
-           int correctindex=nums[j]-1;
-           if(nums[j]>0 && nums[j]<=n  && nums[j]!=nums[correctindex]){
-                int temp=nums[j];
-                nums[j]=nums[correctindex];
-                nums[correctindex]=temp;
-           }
-           else{
-            j++;
-           }
+        Arrays.sort(nums);
+        int ans=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<ans) continue;
+            if(nums[i]==ans) ans++;
+            else if(nums[i]>ans) return ans;
         }
-        for(int k=0;k<n;k++){
-            if(nums[k]!=k+1){
-                return k+1;
-            }
-        }
-        return n+1;
+        return ans;
     }
 }
+
+// int n=nums.length;
+//         int j=0;
+//         while(j<n){
+//            int correctindex=nums[j]-1;
+//            if(nums[j]>0 && nums[j]<=n  && nums[j]!=nums[correctindex]){
+//                 int temp=nums[j];
+//                 nums[j]=nums[correctindex];
+//                 nums[correctindex]=temp;
+//            }
+//            else{
+//             j++;
+//            }
+//         }
+//         for(int k=0;k<n;k++){
+//             if(nums[k]!=k+1){
+//                 return k+1;
+//             }
+//         }
+//         return n+1;
