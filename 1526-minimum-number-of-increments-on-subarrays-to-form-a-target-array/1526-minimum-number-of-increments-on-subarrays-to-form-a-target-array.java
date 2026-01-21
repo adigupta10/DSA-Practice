db297e49fa840f0d814ccbbf55051b334ef93471
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-01-21 15:10:08 +0530

    Time: 5 ms (9.32%), Space: 75.1 MB (13.26%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -2,6 +2,12 @@ class Solution {
     public int minNumberOperations(int[] target) {
         int sum=target[0];
         for(int i=1;i<target.length;i++){
+            if(target[i]<0 && target[i-1]>0){
+                sum+=Math.abs(target[i]);
+            }
+            if(target[i]>0 && target[i-1]<0){
+                sum+=Math.abs(target[i]);
+            }
             if(target[i]>target[i-1]){
                 sum+=target[i]-target[i-1];
             }
