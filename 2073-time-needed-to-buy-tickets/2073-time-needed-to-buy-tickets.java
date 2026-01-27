ca345d8b03a4926dfc2ef9ab3f74b3075bf3470a
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-01-27 21:46:48 +0530

    Time: 11 ms (15.61%), Space: 46.5 MB (5.17%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,15 +1,22 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         Queue<Integer> q=new LinkedList<>();
-        int time=0;
-        for(int i=0;i<tickets.length;i++){
-           if(i<=k){
-            time+=Math.min(tickets[i],tickets[k]);
-           }
-           if(i>k){
-            time+=Math.min(tickets[k]-1,tickets[i]);
-           }
+        int n=tickets.length;
+        for(int i=0;i<n;i++){
+            q.offer(i);
         }
-        return time;
+        int count=0;
+        while(!q.isEmpty()){
+            count++;
+            int curr=q.poll();
+            tickets[curr]--;
+            if(curr==k  && tickets[curr]==0){
+                return count;
+            }
+            if(tickets[curr]!=0){
+                q.offer(curr);
+            }
+        }
+        return count;
     }
 }
