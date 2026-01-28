f2d2c82771066001b7b37724c6e79cf06e27077a
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-01-28 15:05:39 +0530

    Time: 6 ms (100%), Space: 102.6 MB (57.18%) - LeetHub

---
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minOperations(int[] arr) {
+        int n=arr.length;
+        int count=0;
+       for(int j=0;j<=n-3;j++){
+            if(arr[j]!=0){
+                continue;
+            }
+            else{
+                arr[j]=1;
+                arr[j+1]=arr[j+1]==0 ? 1 : 0;
+                arr[j+2]=arr[j+2]==0 ? 1 : 0;
+                count++;
+            }
+        }
+       for(int i : arr){
+        if(i==0) return -1;
+       }
+       return count;
+    }
+}
