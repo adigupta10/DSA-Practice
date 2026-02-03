def21f8cf3105cbb62a60b9e2debaff409ddb9d7
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-02-04 02:30:50 +0530

    Time: 56 ms (49.77%), Space: 105.3 MB (7.78%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.java
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.java
@@ -1,29 +1,36 @@
-class Solution{
-    public int totalSteps(int[] nums){
-        Stack<int[]> st=new Stack<>();
+class Solution {
+    public int totalSteps(int[] nums) {
         int ans=0;
-
-        for(int i=0;i<nums.length;i++){
-            int steps=0;
-
-            while(!st.isEmpty()&&nums[i]>=st.peek()[0]){
-                steps=Math.max(steps,st.pop()[1]);
+        int n=nums.length;
+        Stack<int[]> st=new Stack<>();
+        st.push(new int[]{nums[n-1],0});
+        for(int i=n-2;i>=0;i--){
+            int count=0;
+            while(!st.isEmpty() && nums[i]>st.peek()[0]){
+                count=Math.max(count+1,st.peek()[1]);
+                st.pop();
             }
-
-            if(!st.isEmpty()){
-                steps++;
-            }
-
-            ans=Math.max(ans,steps);
-            st.push(new int[]{nums[i],steps});
+            ans=Math.max(ans,count);
+            st.push(new int[]{nums[i],count});
         }
         return ans;
     }
 }
+        // ArrayList<Integer> l=new ArrayList<>();
+        // for(int x:nums) l.add(x);
+        // int steps=0;
+        // while(true){
+        //     ArrayList<Integer> temp=new ArrayList<>();
+        //     for(int i=1;i<l.size();i++){
+        //         if(l.get(i)<l.get(i-1)){
+        //             temp.add(i);
+        //         }
+        //     }
+        //     if(temp.size()==0) break;
 
-    //     ArrayList<Integer> l=new ArrayList<>();
-    //     while(!st.isEmpty()){
-    //       l.add(st.pop());
-    //     }
-    //     System.out.print(l);
-    // }
+        //     for(int j=temp.size()-1;j>=0;j--){
+        //         l.remove(temp.get(j));
+        //     }
+        //     steps++;
+        // }
+        // return steps;
