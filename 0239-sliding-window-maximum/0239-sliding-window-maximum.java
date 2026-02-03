9722f2a211a0f847295de7aabc6cdcc9d7f42678
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-02-03 11:57:57 +0530

    Time: 28 ms (90.36%), Space: 144.4 MB (76.53%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,22 +1,31 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums==null || nums.length==0) return new int[0];
-        int n = nums.length;
-        int[] result = new int[n-k+1];
-        Deque<Integer> dq = new LinkedList<>();
-
-        for (int i=0;i<n;i++) {
-            while(!dq.isEmpty() && dq.peekFirst()<=i-k){
-                dq.pollFirst();
+        Deque<Integer> dp=new ArrayDeque<>();
+        int n=nums.length;
+        int[] arr=new int[n-k+1];
+        //first window
+        for(int i=0;i<k;i++){
+            while(!dp.isEmpty() && nums[dp.peekLast()] <=nums[i]){
+                dp.pollLast();
             }
-            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
-                dq.pollLast();
-            }
-            dq.offerLast(i);
-            if (i>=k-1){
-                result[i-k+1] = nums[dq.peekFirst()];
-            }
+            dp.offerLast(i);
         }
-        return result;
+        int idx=0;
+        for(int i=k;i<n;i++){
+            arr[idx++]=nums[dp.peekFirst()];
+
+            //out of boundary
+            while(!dp.isEmpty() && dp.peekFirst()<i-k+1){
+                dp.pollFirst();
+            }
+
+            //new element position by removing smaller element
+            while(!dp.isEmpty() && nums[dp.peekLast()]<nums[i]){
+                dp.pollLast();
+            }
+            dp.offerLast(i);
+        }
+        arr[idx]=nums[dp.pollFirst()];
+        return arr;
     }
 }
