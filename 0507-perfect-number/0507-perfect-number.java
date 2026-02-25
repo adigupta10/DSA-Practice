5c8b453d9fc13b1ecf39961643dc8201f8d6e5e9
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-02-26 00:32:00 +0530

    Time: 2079 ms (60.87%), Space: 42.1 MB (63.57%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+        if(n==1) return false;
+       int sum=1;
+        for(int i=2;i<n;i++){
+            if(n%i==0) sum+=i;
+        }
+        if(sum==n) return true;
+        return false;
+    }
+}
