4033c4c18cc7930904d5c7564ad2e5eca2928f09
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-09 01:41:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,26 +1,15 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n=nums[0].length();
         StringBuilder sb=new StringBuilder();
-        //generating all binary string of length n and checking into the map
-        HashSet<String> map=new HashSet<>();
-        for(String s : nums){
-            map.add(s);
-        }
-        String ans=solve(map,"",n);
-        return ans;
-    }
-    public static String solve(HashSet<String> map,String temp,int n){
-        if(temp.length()==n){
-            if(!map.contains(temp)){
-                return temp;
+        int i=0;
+        for(String n: nums){
+            if(n.charAt(i++)=='1'){
+                sb.append('0');
             }
-            return "";
+            else {
+                sb.append('1');
+            }
         }
-       
-        String left=solve(map,temp+"0",n);
-        if(!left.equals("")) return left;
-        String right=solve(map,temp+"1",n);
-        return right;
+        return sb.toString();
     }
 }
