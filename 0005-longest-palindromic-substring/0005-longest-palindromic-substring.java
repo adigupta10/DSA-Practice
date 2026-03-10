3e4af0cf55a5de88a60bd1c5ea3f4d57f4e5ddf5
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-10 16:25:24 +0530

    Time: 259 ms (17.01%), Space: 122.5 MB (5.15%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,23 +1,30 @@
 class Solution {
     String ans="";
-    boolean solve(int i, int j, String s){
-        int l=i;
-        int r=j;
-        while(l<r){
-            if(s.charAt(l)!=s.charAt(r)){
-                return false;
-            }
-            l++;
-            r--;
+    int[][] dp;
+    boolean solve(String s,int i,int j){
+      if(i>=j) return true;
+      if(dp[i][j]!=-1) return dp[i][j]==1;
+      if(s.charAt(i)==s.charAt(j)){
+        if(solve(s,i+1,j-1)){
+            dp[i][j]=1;
+            return true;
         }
-        return true;
+      }
+      dp[i][j]=0;
+      return false;
     }
     public String longestPalindrome(String s) {
         int n=s.length();
+        dp=new int[n][n];
         int len=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+        for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(solve(i,j,s)){
+                if(solve(s,i,j)){
                     if(j-i+1>len){
                         len=j-i+1;
                         ans=s.substring(i,j+1);
