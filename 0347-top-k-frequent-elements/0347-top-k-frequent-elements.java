4c7452283e1e0159fa011d9595bb6804a53ad5e0
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-11 14:34:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,7 +4,12 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
+            if(a[1]!=b[1]){
+                return a[1]-b[1];
+            }
+            return a[0]-b[0];
+        });
         for(int key:map.keySet()){
             pq.offer(new int[]{key,map.get(key)});
             if(pq.size()>k) pq.poll();
