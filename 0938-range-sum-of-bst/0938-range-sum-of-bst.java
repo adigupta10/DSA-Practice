3f8ca0500c3bd5c2e1e66ba493a0a152c5542396
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-14 11:39:52 +0530

    Time: 1 ms (43.13%), Space: 53.8 MB (89.86%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -14,24 +14,49 @@
  * }
  */
 class Solution {
+    int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
-        //bfs solution
-        Queue<TreeNode> q=new LinkedList<>();
-        int sum=0;
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode curr=q.poll();
-            if(curr==null) continue;
-            if(curr.val>=low && curr.val<=high){
-                sum+=curr.val;
-            }
-            if(curr.val>low){
-                q.offer(curr.left);
-            }
-            if(curr.val<high){
-                q.offer(curr.right);
-            }
-        }
+        inorder(root,low,high);
         return sum;
     }
+    public void inorder(TreeNode root, int low, int high){
+        if(root==null) return;
+        inorder(root.left,low,high);
+        if(root.val>=low && root.val<=high) sum+=root.val;
+        inorder(root.right,low,high);
+    }
 }
+
+
+//visiting everynode O(n) 
+//  int sum=0;
+//     public int rangeSumBST(TreeNode root, int low, int high) {
+//         inorder(root,low,high);
+//         return sum;
+//     }
+//     public void inorder(TreeNode root, int low, int high){
+//         if(root==null) return;
+//         inorder(root.left,low,high);
+//         if(root.val>=low && root.val<=high) sum+=root.val;
+//         inorder(root.right,low,high);
+//     }
+
+
+// //bfs solution
+//         Queue<TreeNode> q=new LinkedList<>();
+//         int sum=0;
+//         q.add(root);
+//         while(!q.isEmpty()){
+//             TreeNode curr=q.poll();
+//             if(curr==null) continue;
+//             if(curr.val>=low && curr.val<=high){
+//                 sum+=curr.val;
+//             }
+//             if(curr.val>low){
+//                 q.offer(curr.left);
+//             }
+//             if(curr.val<high){
+//                 q.offer(curr.right);
+//             }
+//         }
+//         return sum;
