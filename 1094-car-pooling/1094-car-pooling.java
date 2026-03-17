8b692ce6196dac39df207dafb61b311515af6f12
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-17 15:08:47 +0530

    Time: 2 ms (66.29%), Space: 46.4 MB (40.01%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,31 +1,17 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        Arrays.sort(trips,(a,b)->Integer.compare(a[1],b[1]));
-        int remainingSeats=capacity;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[0]-b[0]);  // ending and capacity
-        pq.offer(new int[]{trips[0][2],trips[0][0]});
-        if(remainingSeats<trips[0][0]) return false;
-        remainingSeats-=trips[0][0];
-        boolean flag=true;
+        int[] ans=new int[1001];
         for(int[] x:trips){
-            int currP=x[0];
+            int p=x[0];
             int st=x[1];
             int end=x[2];
-
-            if(flag){
-                flag=false;
-                continue;
-            }
-            while(!pq.isEmpty() && st>=pq.peek()[0]){
-                remainingSeats+=pq.peek()[1];
-                pq.poll();
-            }
-
-            if(remainingSeats<currP) return false;
-
-            remainingSeats-=currP;
-
-            pq.offer(new int[]{end,currP});
+            ans[st]+=p;
+            ans[end]-=p;
+        }
+        int max=0;
+        for(int d:ans){
+            max+=d;
+            if(max>capacity) return false;
         }
         return true;
     }
