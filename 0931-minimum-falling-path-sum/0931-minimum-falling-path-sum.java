79b2428374e22ed1b2beb1688069d6b9ea9d0882
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-19 14:21:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -16,3 +16,30 @@ class Solution {
         return ans;
     }
 }
+
+// class Solution {
+//     static int[][] dp;
+//     public int minFallingPathSum(int[][] grid) {
+//         int st=0;
+//         int end=0;
+//         int n=grid.length;
+//         int ans=Integer.MAX_VALUE;
+//         dp=new int[n][n];
+//         for(int[] x:dp){
+//             Arrays.fill(x,-1);
+//         }
+//         for(int j=0;j<n;j++){
+//             ans=Math.min(ans,solve(st,j,grid));
+//         }
+//         return ans;
+//     }
+//     public static int solve(int i,int j,int[][] grid){
+//         if(i>=grid.length || j<0 || j>=grid.length) return (int) 1e9;
+//         if(i==grid.length-1) return grid[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int down=solve(i+1,j,grid);
+//         int left=solve(i+1,j-1,grid);
+//         int right=solve(i+1,j+1,grid);
+//         return dp[i][j]=grid[i][j] + Math.min(down,Math.min(left,right));
+//     }
+// }
