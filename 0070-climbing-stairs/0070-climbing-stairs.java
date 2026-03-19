57f1144ec8ca6162ef1eed69f64dbfae841ce5a7
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-19 10:54:46 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.68%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int[] dp;
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+        Arrays.fill(dp,-1); 
+        return solve(n);
+    }
+    public int solve(int n){
+        if(n==1) return 1;
+        if(n==0) return 1;
+
+        if(dp[n]!=-1) return dp[n];
+
+        dp[n]=solve(n-1) +  solve(n-2); 
+        return dp[n];
+    }
+}
