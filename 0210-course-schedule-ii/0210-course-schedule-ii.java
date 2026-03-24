7aec71bfdb7a5bae580abc71b233746fbca24256
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-24 22:08:35 +0530

    Time: 4 ms (93.28%), Space: 47 MB (41.09%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,43 +1,41 @@
 class Solution {
-    private boolean dfs(int currNode,List<List<Integer>> adj,boolean[] vis,boolean[] rec,List<Integer> topo){
-        vis[currNode]=true;
-        rec[currNode]=true;
-        for(int neigh : adj.get(currNode)){
-            if(!vis[neigh]){
-                if(dfs(neigh,adj,vis,rec,topo)) return true;
-            }
-            else{
-                if(rec[neigh]){
-                    return true;
+    public int[] findOrder(int n, int[][] prerequisites) {
+        ArrayList<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] e:prerequisites){
+            adj.get(e[1]).add(e[0]);
+        }
+        boolean[] vis=new boolean[n];
+        boolean[] rec=new boolean[n];
+        Stack<Integer> st=new Stack<>();
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,rec,st)){
+                    return new int[]{};
                 }
             }
         }
-        topo.add(currNode);
-        rec[currNode]=false;
-        return false;
-    }
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        //dfs and topo sort
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] cell:prerequisites){
-            adj.get(cell[1]).add(cell[0]);
-        } 
-        boolean[] vis=new boolean[numCourses];
-        boolean[] rec=new boolean[numCourses];
-        List<Integer> topo = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,rec,topo)) return new int[0];
-            }
-        }
-        Collections.reverse(topo);
-        int[] ans=new int[numCourses];
-        for(int i=0;i<topo.size();i++){
-            ans[i]=topo.get(i);
+        int[] ans=new int[st.size()];
+        int idx=0;
+        while(!st.isEmpty()){
+            ans[idx++]=st.pop();
         }
         return ans;
+    }
+    public static boolean dfs(int node,ArrayList<List<Integer>> adj,boolean[] vis,boolean[] rec,Stack<Integer> st){
+        vis[node]=true;
+        rec[node]=true;
+        for(int neigh:adj.get(node)){
+            if(!vis[neigh]){
+                if(dfs(neigh,adj,vis,rec,st)) return true;
+            }
+            else if(rec[neigh]) return true;
+        }
+        rec[node]=false;
+        st.push(node);
+        return false;
     }
 }
