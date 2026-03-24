a1955b4688e3a94036813792e1310fa48158d7e8
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-24 10:56:16 +0530

    Time: 8 ms (70.42%), Space: 59 MB (63.75%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,52 +1,35 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        // Your code here
         int n=grid.length;
         int m=grid[0].length;
-        Queue<int[]> q=new LinkedList<>();
+        int cnt=0;
         boolean[][] vis=new boolean[n][m];
-        //row
         for(int i=0;i<n;i++){
-            if(grid[i][0]==1 && !vis[i][0]){
-                q.offer(new int[]{i,0});
-                vis[i][0]=true;
-            }
-            if(grid[i][m-1]==1 && !vis[i][m-1]){
-                q.offer(new int[]{i,m-1});
-                vis[i][m-1]=true;
-            }
-        }
-        for(int j=0;j<m;j++){
-            if(grid[0][j]==1 && !vis[0][j]){
-                q.offer(new int[]{0,j});
-                vis[0][j]=true;
-            }
-            if(grid[n-1][j]==1 && !vis[n-1][j]){
-                q.offer(new int[]{n-1,j});
-                vis[n-1][j]=true;
-            }
-        }
-        int[][] direction={{0,1},{-1,0},{0,-1},{1,0}};
-        while(!q.isEmpty()){
-            int[] cell=q.poll();
-            int r=cell[0];
-            int c=cell[1];
-            for(int[] dir:direction){
-                int nr=r+dir[0];
-                int nc=c+dir[1];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]==1){
-                    vis[nr][nc]=true;
-                    q.offer(new int[]{nr,nc});
-                    
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){  // boundary cell
+                    if(grid[i][j]==1 && !vis[i][j]){
+                        dfs(i,j,grid,vis);
+                    }
                 }
             }
         }
-        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==false) count++;
+                if(!vis[i][j] && grid[i][j]==1) cnt++;
             }
         }
-        return count;
+        return cnt;
+    }
+    public static void dfs(int i,int j, int[][] grid, boolean[][] vis){
+        //check for boundary
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || vis[i][j] || grid[i][j]!=1) return;
+
+         vis[i][j]=true;
+
+         dfs(i+1,j,grid,vis);
+         dfs(i-1,j,grid,vis);
+         dfs(i,j+1,grid,vis);
+         dfs(i,j-1,grid,vis);
+        //calling all four directions
     }
 }
