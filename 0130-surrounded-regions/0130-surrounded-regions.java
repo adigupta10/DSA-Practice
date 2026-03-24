a2e57de6fe973799874cf27297466e68e7502b60
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-24 11:17:15 +0530

    Time: 2 ms (79.74%), Space: 47.8 MB (68.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,51 +1,35 @@
 class Solution {
     public void solve(char[][] board) {
-        Queue<int[]> q=new LinkedList<>();
-        int m=board.length; //row
-        int n=board[0].length; //col
-        boolean[][] vis=new boolean[m][n];   //false values se initialise ho rha hai
-        //row
-        for(int j=0;j<n;j++){
-            if(board[0][j]=='O' && !vis[0][j]){
-                vis[0][j]=true;
-                q.offer(new int[]{0,j});
-            }
-            if(board[m-1][j]=='O' && !vis[m-1][j]){
-                vis[m-1][j]=true;
-                q.offer(new int[]{m-1,j});
-            }
-        }
-        //col
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O' && !vis[i][0]){
-                vis[i][0]=true;
-                q.offer(new int[]{i,0});
-            }
-            if(board[i][n-1]=='O' && !vis[i][n-1]){
-                vis[i][n-1]=true;
-                q.offer(new int[]{i,n-1});
-            }
-        }
-        int[][] direction={{0,1},{-1,0},{0,-1},{1,0}};
-        while(!q.isEmpty()){
-            int[] cell=q.poll();
-            int r=cell[0];
-            int c=cell[1];
-            for(int[] dir:direction){
-                int nr=r+dir[0];
-                int nc=c+dir[1];
-                if(nc>=0  && nc<n  && nr>=0 && nr<m && board[nr][nc]=='O' && vis[nr][nc]!=true){
-                    vis[nr][nc]=true;
-                    q.offer(new int[]{nr,nc});
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(board[i][j]=='O'){
+                        dfs(i,j,board);
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='O' && vis[i][j]==false){
-                    board[i][j]='X';
-                }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='#') board[i][j]='O';
             }
         }
+    }
+
+    public void dfs(int i,int j,char[][] b){
+        if(i<0 || j<0 || i>=b.length || j>=b[0].length || b[i][j]!='O')
+            return;
+
+        b[i][j]='#';
+
+        dfs(i+1,j,b);
+        dfs(i-1,j,b);
+        dfs(i,j+1,b);
+        dfs(i,j-1,b);
     }
 }
