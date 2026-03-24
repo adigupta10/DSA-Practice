303f15ecd113958d15adb8a65568727a1bb17a72
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-24 11:41:32 +0530

    Time: 2 ms (65.68%), Space: 46.6 MB (45.17%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,36 +1,27 @@
 class Solution {
-    int n;
-    int m;
-    int area;
     public int maxAreaOfIsland(int[][] grid) {
-        int maxi=0;
-        n=grid.length;
-        m=grid[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
         boolean[][] vis=new boolean[n][m];
+        int maxi=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1 && !vis[i][j]){
-                    area=dfs(i,j,vis,grid);
-                    maxi=Math.max(maxi,area);
-                    
+                    int val=dfs(i,j,grid,vis);
+                    maxi=Math.max(maxi,val);
                 }
             }
         }
         return maxi;
     }
-    private int dfs(int i,int j,boolean[][] vis,int[][] grid){
-
-        if(i<0 || j<0 || i>=n || j>=m || vis[i][j] || grid[i][j]!=1){
-            return 0;
-        }
-        area=1;
+    public static int dfs(int i,int j,int[][] grid,boolean[][] vis){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!=1 || vis[i][j]) return 0; 
         vis[i][j]=true;
-
-        area+=dfs(i+1,j,vis,grid);
-        area+=dfs(i-1,j,vis,grid);
-        area+=dfs(i,j-1,vis,grid);
-        area+=dfs(i,j+1,vis,grid);
+        int area=1;
+        area+=dfs(i+1,j,grid,vis);
+        area+=dfs(i-1,j,grid,vis);
+        area+=dfs(i,j+1,grid,vis);
+        area+=dfs(i,j-1,grid,vis);
         return area;
     }
 }
-    
