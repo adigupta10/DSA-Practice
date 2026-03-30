1f69878c25c141f3a1fb38a214fe4731d0c53208
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-03-30 15:40:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,17 @@
 class Solution {
-    static int[][] dp;
     public int lengthOfLIS(int[] nums) {
-        dp=new int[nums.length][nums.length+1];
-        for(int[] x:dp){
-            Arrays.fill(x,-1);
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        int maxLis=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                    maxLis=Math.max(maxLis,dp[i]);
+                }
+            }
         }
-        int ans=solve(0,-1,nums);
-        return ans;
-    }
-    public int solve(int i, int prev, int[] nums){
-        if(i>=nums.length) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int notPick=solve(i+1,prev,nums);
-        int pick=0;
-        if(prev==-1 || nums[prev]<nums[i]){
-            pick=1+ solve(i+1,i,nums);
-        }
-        return dp[i][prev+1]=Math.max(pick,notPick);
+        return maxLis;
     }
 }
