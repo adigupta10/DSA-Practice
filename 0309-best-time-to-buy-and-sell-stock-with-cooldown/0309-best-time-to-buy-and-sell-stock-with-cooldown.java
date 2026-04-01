e932454d152306b598c6f9ed135f7e11b358926c
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-01 15:34:05 +0530

    Time: 1 ms (70.85%), Space: 43.6 MB (39.85%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,27 @@
+class Solution {
+    static int[][] dp;
+    public int maxProfit(int[] arr) {
+        // buy==0 and sell==1
+        int n=arr.length;
+        dp=new int[n][2];
+        for(int[] x: dp) Arrays.fill(x,-1);
+        int ans=solve(0,0,arr);
+        return ans;
+    }
+public static int solve(int curr, int state, int[] arr){
+    if(curr>=arr.length) return 0;
+    if(dp[curr][state]!=-1) return dp[curr][state];
+    int profit=0;
+    if(state==0){
+        profit=Math.max(
+            -arr[curr] + solve(curr+1,1,arr),
+                0 +     solve(curr+1,0,arr) );
+    }
+    else{
+        profit=Math.max(
+            arr[curr] + solve(curr+2,0,arr),
+                0 +     solve(curr+1,1,arr) );
+    }
+    return dp[curr][state]=profit;
+    }
+}
