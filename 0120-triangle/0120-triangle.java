133deee4eb148464da5aca4f710b61a29d834559
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-04 19:58:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,16 +1,31 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> t) {
-        List<List<Integer>> l=new ArrayList<>();
         int n=t.size();
-        for(int i=0;i<n;i++){
-            l.add(new ArrayList<>(t.get(i)));
-        }
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                int mini=l.get(i).get(j) + Math.min(l.get(i+1).get(j),l.get(i+1).get(j+1));
-               l.get(i).set(j,mini);
+                int mini=t.get(i).get(j) + Math.min(t.get(i+1).get(j),t.get(i+1).get(j+1));
+               t.get(i).set(j,mini);
             }
         }
-        return l.get(0).get(0);
+        return t.get(0).get(0);
     }
 }
+
+
+
+
+//  // List<List<Integer>> l=new ArrayList<>();
+//         int n=t.size();
+//         // for(List<Integer> x:t){
+//         //     l.add(new ArrayList<>(x));
+//         // }
+//         // for(int i=0;i<n;i++){
+//         //     l.add(new ArrayList<>(t.get(i)));
+//         // }
+//         for(int i=n-2;i>=0;i--){
+//             for(int j=0;j<=i;j++){
+//                 int mini=l.get(i).get(j) + Math.min(l.get(i+1).get(j),l.get(i+1).get(j+1));
+//                l.get(i).set(j,mini);
+//             }
+//         }
+//         return l.get(0).get(0);
