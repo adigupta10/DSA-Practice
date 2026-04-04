844feb7c28ac43f858f665a73c29eef31e0c9041
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-04 20:06:19 +0530

    Time: 1 ms (99.82%), Space: 50.6 MB (15.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,19 +1,23 @@
 class Solution {
+    static int[][] dp;
     public int minPathSum(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int[][] cost =new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0)  cost[0][0]=grid[0][0];
-                else{
-                    int left=(j>0) ? cost[i][j-1] :  Integer.MAX_VALUE ;
-                    int up=(i>0) ? cost[i-1][j] :  Integer.MAX_VALUE ;
+        dp=new int[n][m];
+        for(int[] x:dp) Arrays.fill(x,-1);
+        int ans=solve(0,0,n,m,grid);
+        return ans;
+    }
 
-                    cost[i][j]=grid[i][j] + Math.min(left,up);
-                }
-            }
+     public static int solve(int i, int j, int n , int m,int[][] grid){
+        if(i>=n || j>=m) return (int)1e9;
+        if(i==n-1 && j==m-1){
+            return dp[i][j]=grid[i][j];
         }
-        return cost[n-1][m-1];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right= grid[i][j] +solve(i,j+1,n,m,grid);
+        int down= grid[i][j] +solve(i+1,j,n,m,grid);
+        return dp[i][j]= Math.min(right,down);
+
     }
 }
