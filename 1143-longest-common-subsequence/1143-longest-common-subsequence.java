a769328e216d963789fc45d96baef8f9ab0d3fe6
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-08 18:10:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,20 +5,16 @@ class Solution {
         int m=s2.length();
         dp=new int[n][m];
         for(int[] x:dp) Arrays.fill(x,-1);
-      return solve(0,0,s1,s2);  
+        return solve(0,0,s1,s2);  
     }
     public static int solve(int i ,int j,String s1,String s2){
         if(i>=s1.length() || j>=s2.length()) return 0;
+
         if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
+
         if(s1.charAt(i)==s2.charAt(j)){
-           ans=1+ solve(i+1,j+1,s1,s2);
+           return 1+ solve(i+1,j+1,s1,s2);
         }
-        else{
-            int a=solve(i+1,j,s1,s2);
-            int b=solve(i,j+1,s1,s2);
-            return dp[i][j]=Math.max(a,b);
-        }
-        return dp[i][j]=ans;
+        return dp[i][j]=Math.max(solve(i+1,j,s1,s2),solve(i,j+1,s1,s2));
     }
 }
