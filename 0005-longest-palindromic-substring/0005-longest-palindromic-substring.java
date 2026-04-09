0a3f54adf5e16db77add8e6cd39e50b88d86342b
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-09 11:01:25 +0530

    Time: 206 ms (18.43%), Space: 48.1 MB (10.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,34 @@
 class Solution {
-    String ans="";
-    int[][] dp;
-    boolean solve(String s,int i,int j){
-      if(i>=j) return true;
-      if(dp[i][j]!=-1) return dp[i][j]==1;
-      if(s.charAt(i)==s.charAt(j)){
-        if(solve(s,i+1,j-1)){
-            dp[i][j]=1;
-            return true;
-        }
-      }
-      dp[i][j]=0;
-      return false;
-    }
     public String longestPalindrome(String s) {
         int n=s.length();
-        dp=new int[n][n];
-        int len=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
+        boolean[][] dp=new boolean[n][n];
+        int maxLen=1;
+        int idx=0;
+        for(int i=0;i<n;i++){   //diagonal elements
+            dp[i][i]=true;
         }
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s,i,j)){
-                    if(j-i+1>len){
-                        len=j-i+1;
-                        ans=s.substring(i,j+1);
+        for(int L=2;L<=n;L++){
+            for(int i=0; i+L-1<n;i++){
+                int j=i+L-1;
+
+                if((s.charAt(i)==s.charAt(j)) && L==2 ) {
+                    dp[i][j]=true;
+                    maxLen=2;
+                    idx=i;
+                }
+                //more than 2 len
+                else if((s.charAt(i)==s.charAt(j)) && dp[i+1][j-1]==true ){
+                    dp[i][j]=true;
+                    if(j-i+1>maxLen){
+                        maxLen=j-i+1;
+                        idx=i;
                     }
+                }
+                else{
+                    dp[i][j]=false;
                 }
             }
         }
-        return ans;
+        return s.substring(idx,idx+maxLen);
     }
 }
