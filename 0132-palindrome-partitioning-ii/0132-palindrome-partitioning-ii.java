9a28b48b6983b3b6d4b2bc32158bf4f395ebdac8
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-09 15:40:56 +0530

    Time: 2885 ms (5.02%), Space: 83.1 MB (5.17%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,37 +1,33 @@
 class Solution {
+    static int n;
+    static int[][] dp;
     public int minCut(String s) {
-        int n=s.length();
-        boolean[][] dp=new boolean[n][n];
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;     // 1 len String ex- "a" , "d" , "s" ;
+        n=s.length();
+        dp=new int[n][n];
+        for(int[]x:dp) Arrays.fill(x,-1);
+        return solve(0,n-1,s);
+    }
+    public static int solve(int i,int j,String s){
+        //base case
+        if(i>=n) return 0;
+        if(isPal(i,j,s)) return dp[i][j]=0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int mini=Integer.MAX_VALUE;
+        for(int k=i;k<j;k++){
+            if(isPal(i,k,s)){
+                int cuts =1+ solve(k+1,j,s);
+                mini=Math.min(mini,cuts);
+            }  
         }
-        //filling the boolean dp
-        for(int L=2;L<=n;L++){
-            for(int i=0;i+L-1<n;i++){
-                int j=i+L-1;
-                if((s.charAt(i)==s.charAt(j)) && L==2){  //ex- "aa" ,"dd"
-                    dp[i][j]=true;
-                }
-                else if(( s.charAt(i)==s.charAt(j)) && dp[i+1][j-1]==true){
-                    dp[i][j]=true;
-                }
-            }
-        }
-        // no. of cuts dp
-        int[] t=new int[n];
-        Arrays.fill(t,Integer.MAX_VALUE);
-        for(int i=0;i<n;i++){
-            if(dp[0][i]==true){  // s[0...i] true hai => palindrome means no cuts
-                t[i]=0;
-            }
-            else {
-                for(int k=0;k<i;k++){
-                    if(dp[k+1][i]==true  && 1 + t[k] < t[i]){
-                        t[i]=t[k]+1;
-                    }
-                }
-            }
-        }
-        return t[n-1];
+        return dp[i][j]=mini;
+    }
+    public static boolean isPal(int i,int j,String s){
+        if(i>j) return true;
+
+        if(s.charAt(i)==s.charAt(j)) return isPal(i+1,j-1,s);
+
+        return false;
     }
 }
