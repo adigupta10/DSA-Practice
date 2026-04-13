599d0350bf99c2a344a37dc70e42863df34faccd
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-13 11:36:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean canThreePartsEqualSum(int[] arr) {
         int sum=0;
-        int n=arr.length;
         for(int x:arr) sum+=x;
-        int fr=0;
-        for(int i=0;i<n-2;i++){
-            int temp=sum;
-            fr+=arr[i];
-            int sc=0;
-            for(int j=i+1;j<n-1;j++){
-                sc+=arr[j];
-                if(fr==sc){
-                    temp-=(fr+sc);
-                    if(temp==fr) return true;
-                }
+        if(sum%3!=0) return false;
+        int n=arr.length;
+
+        int target=sum/3;
+        int currSum=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            currSum+=arr[i];
+            if(target==currSum){
+                currSum=0;
+                count++;
+                if(count==2) return true;
             }
         }
         return false;
