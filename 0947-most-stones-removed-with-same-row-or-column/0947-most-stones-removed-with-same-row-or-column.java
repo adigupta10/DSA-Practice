6c1ba47f469ad5e3d021e87187db4346db92fa60
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-04-14 15:58:22 +0530

    Time: 47 ms (13.93%), Space: 46.6 MB (82.39%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,44 +1,22 @@
 class Solution {
-     static int[] rank;
-        static int[] parent;
     public int removeStones(int[][] stones) {
         int n=stones.length;
-       rank=new int[n]; 
-        parent=new int[n]; 
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            rank[i]=0;
-        }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(stones[i][0]==stones[j][0] ||stones[i][1]==stones[j][1] ){
-                    int x=find(i,parent);
-                    int y=find(j,parent);
-                    if(x!=y) union(x,y);
-                }
-            }
-        }
+        boolean[] vis=new boolean[n];
         int grp=0;
         for(int i=0;i<n;i++){
-            if(parent[i]==i) grp++;
+            if(!vis[i]){
+                dfs(i,stones,vis);
+                grp++;
+            }
         }
         return n-grp;
     }
-    //union and find
-    public static int find(int x,int[] parent){
-        if(parent[x]==x) return x;
-        return parent[x]=find(parent[x],parent); 
-    }
-    public static void union(int x,int y){
-        if(rank[x]>rank[y]){
-            parent[y]=x;
-        }
-        else if(rank[y]>rank[x]){
-            parent[x]=y;
-        }
-        else{
-            parent[x]=y;
-            rank[y]+=1;
+    public static void dfs(int i,int[][] stones,boolean[] vis){
+        vis[i]=true;
+        for(int j=0;j<stones.length;j++){
+            if(!vis[j] && (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])){
+                dfs(j,stones,vis);
+            }
         }
     }
 }
