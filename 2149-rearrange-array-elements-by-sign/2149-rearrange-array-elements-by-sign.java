081ec258b00e92d303e28135d2ccc4cc336d4d42
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-06-19 13:39:38 +0530

    Time: 4 ms (41.24%), Space: 176.6 MB (6.74%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,28 @@
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        int odd=1;
-        int even=0;
-        int[] arr=new int[nums.length];
-        for(int x:nums){
-            if(x<0){
-                arr[odd]=x;
-                odd+=2;
+    public int[] rearrangeArray(int[] arr) {
+        int[] neg=new int[arr.length/2];
+        int[] pos=new int[arr.length/2];
+        int nidx=0;
+        int pidx=0;
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            if(curr<0){
+                neg[nidx++]=curr;
             }
             else{
-                arr[even]=x;
-                even+=2;
+                pos[pidx++]=curr;
             }
         }
-        return arr;
+        int[] res=new int[arr.length];
+        pidx=0;
+        nidx=0;
+        for(int i=0;i<arr.length;i+=2){
+            res[i]=pos[pidx];
+            pidx++;
+            res[i+1]=neg[nidx];
+            nidx++;
+
+        }
+        return res;
     }
 }
