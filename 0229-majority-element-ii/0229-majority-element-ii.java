9b04ca4faa1193016a54b0a28043fa2381c5f405
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-06-21 12:52:16 +0530

    Time: 3 ms (72.68%), Space: 52.9 MB (20.96%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int x:nums){
-            map.put(x,map.getOrDefault(x,0)+1);
-        } 
-        for(int key:map.keySet()){
-            if(map.get(key)>(nums.length/3)){
-                ans.add(key);
+        int m1=-1; int m2=-1;
+        int c1=0; int c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==m1){
+                c1++;
             }
+            else if(nums[i]==m2){
+                c2++;
+            }
+            else if(c1==0){
+                c1=1;
+                m1=nums[i];
+            }
+            else if(c2==0){
+                c2=1;
+                m2=nums[i];
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }  
+        ArrayList<Integer> l=new ArrayList<>();
+        int countm1=0;
+        int countm2=0;
+        for(int i:nums){
+            if(i==m1) countm1++;
+            if(i==m2) countm2++;
+        }  
+        if(countm1>(nums.length/3)){
+            l.add(m1);
         }
-        return ans;
+        if(countm2>(nums.length/3)){
+            l.add(m2);
+        }
+        if(m1==m2){
+            l.remove(0);
+        }
+        return l;      
     }
 }
