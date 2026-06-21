67b09bcbeccbd74edf24c627e22c15a5e54c9ab8
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-06-21 15:04:19 +0530

    Time: 20 ms (64.85%), Space: 45.7 MB (62.27%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,21 +1,32 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
-        HashSet<List<Integer>> set=new HashSet<>();
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        int n=arr.length;
+        Arrays.sort(arr);
+         List<List<Integer>> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
             for(int j=i+1;j<n;j++){
-                HashSet<Long> seen=new HashSet<>();
-                for(int k=j+1;k<n;k++){
-                    long needed=(long) target-nums[i]-nums[j]-nums[k];
-                    if(seen.contains(needed)){
-                        List<Integer> quad=Arrays.asList(nums[i],nums[j],nums[k],(int)needed);
-                        Collections.sort(quad);
-                        set.add(quad);
+                if(j>i+1 && arr[j]==arr[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=(long) arr[i]+arr[j]+arr[k]+arr[l];
+                    if(sum==target){
+                        ans.add(Arrays.asList(arr[i],arr[j],arr[k],arr[l]));
+                        k++;
+                        l--;
+                        while(k<l && arr[k]==arr[k-1]) k++;
+                        while(k<l && arr[l]==arr[l+1]) l--;
                     }
-                    seen.add((long) nums[k]);
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
                 }
             }
         }
-        return new ArrayList<>(set);
+        return ans;
     }
 }
