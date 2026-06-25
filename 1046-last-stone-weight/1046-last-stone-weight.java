c2b6f1d45d2debacf77f05d3fd2c2ad394ef9883
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-06-25 10:09:32 +0530

    Time: 1 ms (99.43%), Space: 43 MB (15.38%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,15 +1,32 @@
 class Solution {
-    public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)-> b-a);
-        for(int x:stones) pq.add(x);
-        while(pq.size()>1){
-            int m1=pq.poll();
-            int m2=pq.poll();
-            if(m1!=m2){
-                pq.add(m1-m2);
+    public static int solve(ArrayList<Integer> l){
+        int ans=0;
+        for(int i=1;i<l.size();i++){
+            if(l.get(i)>=l.get(ans)){
+                ans=i;
             }
         }
-        
-        return pq.size()==1 ? pq.poll() : 0;
+        return ans;
+
+    }
+    public int lastStoneWeight(int[] stones) {
+        ArrayList<Integer> l=new ArrayList<>();
+        for(int x:stones){
+            l.add(x);
+        }
+        if(l.size()==1) return 1;
+        while(l.size()>1){
+            int m1=solve(l);
+            int x=l.get(m1);
+            l.remove(m1);
+            int m2=solve(l);
+            int y=l.get(m2);
+            l.remove(m2);
+
+            if(x!=y){
+               l.add(x-y);
+            }
+        }
+        return l.size()==0 ? 0 : l.get(0);
     }
 }
