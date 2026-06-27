8ad016f53eb59de9ab65c2fc64a3fbf82272586a
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-06-27 14:01:58 +0530

    Time: 12 ms (86.54%), Space: 47.5 MB (15.47%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -4,13 +4,25 @@ class Solution {
         for(char ch:s.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        ArrayList<Character> list=new ArrayList<>(map.keySet());
-        Collections.sort(list,(a,b)-> map.get(b)-map.get(a));
-        StringBuilder sb=new StringBuilder();
-        for(char c:list){
+        ArrayList<Character>[] bucket=new ArrayList[s.length()+1];
+        for(char c:map.keySet()){
             int freq=map.get(c);
-            while(freq-->0){
-                sb.append(c);
+            if(bucket[freq]==null){
+                bucket[freq]=new ArrayList<>();
+            }
+            bucket[freq].add(c);
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=bucket.length-1;i>=0;i--){
+            if(bucket[i]!=null){
+                for(char ch:bucket[i]){
+                    int freq=map.get(ch);
+                    // bucket[i] is itself the freq
+                    // for(int j=0;j<i;j++)
+                    while(freq-->0){
+                        sb.append(ch);
+                    }
+                }
             }
         }
         return sb.toString();
