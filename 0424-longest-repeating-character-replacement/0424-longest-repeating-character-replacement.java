3ca181cdc56fee8530d380abf5bda5ce8fb6ee75
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-07-01 17:55:58 +0530

    Time: 6 ms (98.93%), Space: 46.1 MB (78.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,50 +1,26 @@
 class Solution {
-    private int getMax(HashMap<Character,Integer> map){
-        int value=0;
-        for(int f:map.values()){
-            value=Math.max(value,f);
-        }
-        return value;
-    }
     public int characterReplacement(String s, int k) {
-        int l=0; int ans=0;
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int r=0;r<s.length();r++){
-            char ch=s.charAt(r);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            int maxfreq=getMax(map);
-            int windowsize=r-l+1;
-            if(windowsize-maxfreq<=k){
-                ans=Math.max(ans,windowsize);
-            }
-            else{
-                //shrinking the window;
-                char leftchar=s.charAt(l);
-                map.put(leftchar,map.get(leftchar)-1);
-                if(map.get(leftchar)==0){
-                    map.remove(leftchar);
+        int[] freq=new int[26];
+        int left=0;
+        int right=0;
+        int maxi=0;
+        int maxfreq=0;
+        while(right<s.length()){
+            char ch=s.charAt(right);
+            freq[ch-'A']++;
+
+            maxfreq=Math.max(maxfreq,freq[ch-'A']);
+
+            while(right-left+1 - maxfreq > k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+                for(int i=0;i<26;i++){
+                    maxfreq=Math.max(maxfreq,freq[i]);
                 }
-                l++;
             }
+            maxi=Math.max(maxi,right-left+1);
+            right++;
         }
-        return ans;
+        return maxi;
     }
 }
-
-
-
-// int ans=0;
-// for(int i=0;i<s.length();i++){
-//     int[] freq=new int[26];
-//     for(int j=i;j<s.length();j++){
-//         char ch=s.charAt(j);
-//         freq[ch-'A']++;
-//         int maxfreq=0;
-//         for(int f:freq) maxfreq=Math.max(f,maxfreq);
-//         int len=j-i+1;
-//         if(len-maxfreq<=k){
-//             ans=Math.max(len,ans);
-//         }
-//     }
-// }
-// return ans;
