5121ad3c9659c474d800244dd10c544870355743
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-07-15 11:14:21 +0530

    Time: 1 ms (83.11%), Space: 42.7 MB (20.94%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int sumodd=0;
+        int sumeven=0;
+        for(int i=1;i<=n;i++){
+            sumodd+=(2*i-1);
+            sumeven+=i*2;
+        }
+        return gcd(sumodd,sumeven);
+    }
+    public static int gcd(int a, int b){
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+}
