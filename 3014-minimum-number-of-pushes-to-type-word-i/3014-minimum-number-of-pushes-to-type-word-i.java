c5810b86856fdeeab8ddaaac4e34e1916c775660
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-07-31 17:56:40 +0530

    Time: 2 ms (21.26%), Space: 43.5 MB (8.94%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,17 +1,15 @@
 class Solution {
     public int minimumPushes(String word) {
         int sum=0;
-        int press=1;
-        int idx=1;
-        int i=0;
-        int n=word.length();
-        while(i+8<n){
-            sum+=8*idx;
-            i=i+8;
-            idx++;
-        }
-        if(i<n){
-            sum+=(n-1-i+1)*idx;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int assign_key=2;
+        for(int i=0;i<word.length();i++){
+            if(assign_key>9){
+                assign_key=2;
+            }
+            map.put(assign_key,map.getOrDefault(assign_key,0)+1);
+            sum+=map.get(assign_key);
+            assign_key++;
         }
         return sum;
     }
