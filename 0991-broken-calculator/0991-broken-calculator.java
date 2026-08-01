a3563bd8bcf4007f05fdde8886fae006c7a30715
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-08-01 11:50:34 +0530

    Time: 0 ms (100%), Space: 42.3 MB (7.56%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        // we have to go in reverse order
+        if(startValue==target) return 0;
+        if(startValue>target) return startValue-target;
+        if(target%2==0){
+            return 1+brokenCalc(startValue,target/2);
+        }
+        else{
+            return 1+brokenCalc(startValue,target+1);
+        }
+    }
+}
