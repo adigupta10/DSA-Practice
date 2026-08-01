847843ca95cd191aecd1cc35d1e02436d91a2ca7
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-08-01 16:21:47 +0530

    Time: 28 ms (90.7%), Space: 144.3 MB (72.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,31 +1,75 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dp=new ArrayDeque<>();
+        Deque<Integer> dq=new ArrayDeque<>();
         int n=nums.length;
         int[] arr=new int[n-k+1];
-        //first window
         for(int i=0;i<k;i++){
-            while(!dp.isEmpty() && nums[dp.peekLast()] <=nums[i]){
-                dp.pollLast();
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
+                dq.pollLast();
             }
-            dp.offerLast(i);
+            dq.offerLast(i);
         }
-        int idx=0;
-        for(int i=k;i<n;i++){
-            arr[idx++]=nums[dp.peekFirst()];
 
-            //out of boundary
-            while(!dp.isEmpty() && dp.peekFirst()<i-k+1){
-                dp.pollFirst();
+        int idx=1;
+        int left=1;
+        arr[0]=nums[dq.peekFirst()];
+
+        for(int right=k;right<nums.length;right++){
+            //out of bound
+            while(!dq.isEmpty() && dq.peekFirst()<left){
+                dq.pollFirst();
+            }
+            //smaller ele in dq
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[right]){
+                dq.pollLast();
             }
 
-            //new element position by removing smaller element
-            while(!dp.isEmpty() && nums[dp.peekLast()]<nums[i]){
-                dp.pollLast();
-            }
-            dp.offerLast(i);
+            dq.offerLast(right);
+            left++;
+            arr[idx++]=nums[dq.peekFirst()];
         }
-        arr[idx]=nums[dp.pollFirst()];
         return arr;
     }
 }
+
+
+
+
+// class Solution {
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+
+//         int n = nums.length;
+//         int[] ans = new int[n - k + 1];
+
+//         // Max Heap -> {value, index}
+//         PriorityQueue<int[]> pq = new PriorityQueue<>(
+//                 (a, b) -> b[0] - a[0]
+//         );
+
+//         int left = 0;
+
+//         // First window
+//         for (int i = 0; i < k; i++) {
+//             pq.offer(new int[]{nums[i], i});
+//         }
+
+//         ans[0] = pq.peek()[0];
+
+//         // Slide the window
+//         for (int right = k; right < n; right++) {
+
+//             pq.offer(new int[]{nums[right], right});
+
+//             left++;
+
+//             // Remove all elements that are left of the window
+//             while (pq.peek()[1] < left) {
+//                 pq.poll();
+//             }
+
+//             ans[left] = pq.peek()[0];
+//         }
+
+//         return ans;
+//     }
+// }
