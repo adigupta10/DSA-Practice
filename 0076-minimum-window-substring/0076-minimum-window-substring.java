7912f253d633a38ba8086caba63d11dd0337805c
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-08-01 17:07:04 +0530

    Time: 46 ms (18.99%), Space: 46.4 MB (59.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,105 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int k=0;k<t.length();k++){
-            char ch=t.charAt(k);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] freq1=new int[256];
+        int[] freq2=new int[256];
+
+        for(char ch:t.toCharArray()){
+            freq2[ch]++;
         }
-        int unique=map.size();
-        int left=0; int right=0;
+        int start=0;
+        int left=0;
         int minlen=Integer.MAX_VALUE;
-        int startIndex=-1;
-        int n=s.length();
-        while(right<n){
-            char ch=s.charAt(right);
-                if(map.containsKey(ch)){
-                    map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==0){
-                    unique--;
+        for(int right=0;right<s.length();right++){
+            freq1[s.charAt(right)]++;
+            while(check(freq1,freq2)){
+                if(right-left+1<minlen){
+                    minlen=right-left+1;
+                    start=left;
                 }
-            }
-            //shrinking window
-            while(unique==0){
-                int len=right-left+1;
-                if(len<minlen){
-                    startIndex=left;
-                    minlen=len;
-                }
-                ch=s.charAt(left);
-                if(map.containsKey(ch)){
-                    map.put(ch,map.get(ch)+1);
-                    if(map.get(ch)>0) unique++;
-                }
+                freq1[s.charAt(left)]--;
                 left++;
             }
-            right++;
         }
-        if(startIndex==-1) return "";
-        return s.substring(startIndex,startIndex+minlen);
+        return minlen==Integer.MAX_VALUE ? "" : s.substring(start,start+minlen);  
+    }
+    public static boolean check(int[] freq1,int[] freq2){
+        for(int i=0;i<256;i++){
+            if(freq1[i]<freq2[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
-
-// StringBuilder ans=new StringBuilder();
-//         ans.append(s+"1");
-//         for(int i=0;i<s.length();i++){
-//             HashMap<Character,Integer> map=new HashMap<>();
-//             for(char j:t.toCharArray()){
-//                 map.put(j,map.getOrDefault(j,0)+1);
-//             }
-//             StringBuilder temp=new StringBuilder();
-//             for(int j=i;j<s.length();j++){
-//                 char curr=s.charAt(j);
-//                 temp.append(curr);
-//                 if(map.containsKey(curr)){
-//                     map.put(curr,map.get(curr)-1);
-//                     if(map.get(curr)==0){
-//                         map.remove(curr);
-//                     }
-//                 }
-//                 if(map.size()==0){
-//                     if(temp.length()<ans.length()){
-//                         ans.setLength(0);
-//                         ans.append(temp);
-//                     }
-//                    break;
-//                 }
-//             }
-//         }
-//         if(ans.length()==s.length()+1) return "";
-//         return ans.toString();
-
-
-// another answer
-// class Solution {
-//     public String minWindow(String s, String t) {
-//         int len=Integer.MAX_VALUE;
-//         int startIndex=-1;
-//         for(int i=0;i<s.length();i++){
-//             HashMap<Character,Integer> map=new HashMap<>();
-//             int count=t.length();
-//             for(int k=0;k<t.length();k++){
-//                 char ch=t.charAt(k);
-//                 map.put(ch,map.getOrDefault(ch,0)+1);
-//             }
-//             for(int j=i;j<s.length();j++){
-//                 char ch=s.charAt(j);
-//                 if(map.containsKey(ch)){
-//                     map.put(ch,map.get(ch)-1);
-//                     if(map.get(ch)==0){
-//                         map.remove(ch);
-//                     }
-//                     count--;
-//                 }
-//                 if(count==0){
-//                     if(j-i+1<len){
-//                         len=j-i+1;
-//                         startIndex=i;
-//                     }
-//                     break;
-//                 }
-//             }
-//         }
-//         if(startIndex==-1) return "";
-//         return s.substring(startIndex,startIndex+len);
-//     }
-// }
