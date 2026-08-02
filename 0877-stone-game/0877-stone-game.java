c9971dd6cb0105db823092f3f2b31e9049f5227a
Aditya Kumar Gupta <aditya.gupta4_cs23@gla.ac.in>
2026-08-02 13:23:23 +0530

    Time: 8 ms (18.78%), Space: 47.8 MB (8.21%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,21 @@
 class Solution {
+    static int[][] dp;
     public boolean stoneGame(int[] piles) {
-        return true;
+         int sum=0;
+         int n=piles.length;
+         dp=new int[n+1][n+1];
+         for(int[] x:dp) Arrays.fill(x,-1);
+         for(int x:piles) sum+=x;
+         int alice=solve(piles,0,n-1);
+         return alice>sum/2;
+    }
+    public static int solve(int[] piles,int i,int j){
+        if(i>j) return 0;
+        if(i==j) return dp[i][j]=piles[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take_i=piles[i] + Math.min(solve(piles,i+2,j),solve(piles,i+1,j-1));
+        int take_j=piles[j] + Math.min(solve(piles,i,j-2),solve(piles,i+1,j-1));
+
+        return dp[i][j]=Math.max(take_i,take_j);
     }
 }
